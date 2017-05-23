d09bf55bf4a42b248d7c69ba187bdd8de9cbdf61
Rob pickering <rob@ipcortex.co.uk>
2017-05-23 18:05:08 +0100

    First pass working against 6.2.14 PBX

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var IPCortex = require('ipcortex-pabx');
 const config = require('node-config-env').create(__dirname + '/config', 'prod');
 
-
+// Read config/pabx.js to get host and login credentials
 console.log(pabx = config.get('pabx'));
+
+
 IPCortex.PBX.Auth.setHost('https://' + pabx.host);
+
 IPCortex.PBX.Auth.login({
   username: pabx.user,
   password: pabx.password
@@ -11,8 +14,8 @@ IPCortex.PBX.Auth.login({
   console.log('Login successful');
   IPCortex.PBX.startFeed().then(function() {
     console.log('Live data feed started');
-    IPCortex.Types.Queue.addListener('update', callbackCall);
-    // Do stuff here
+    // Once we are live, add a listener for all queues
+    IPCortex.Types.Queue.addListener('update', cbQueue);
   }, function() {
     console.log('Live data feed failed');
   });
@@ -20,77 +23,135 @@ IPCortex.PBX.Auth.login({
   console.log('Login failed');
 });
 
-function cbQueue(queue) {
-  console.log('got Queue', queue.name, queue.depth, queue.calls);
 
+// Queue listener, called whenever a queue changes
+
+function cbQueue(queue) {
+  /*
+   * I run this on a test PBX with just one queue. In a real implementation you would restrict this
+   * to queues that we are interested in
+   */
+
+  console.log('got Queue change:', queue.name, queue.depth);
+  // Iterate of the calls in the queue and add a listener to any we don't already know about
   for (var c in queue.calls) {
     call = queue.calls[c];
-    console.log('got Call', call.id, call.state, call.name);
-    call.addListener('update', cbCall)
+    if (!callState[call.id]) {
+      call.addListener('update', cbCall);
+      // prime the pump on calls arriving by calling the update callback
+      cbCall.apply(call);
+    }
 
   }
 
 }
 
-// Called as getdtmf#NN#XX#YY#ZZZZ - NN = msgno, XX = timeout, YY = maxdigits, ZZZZ = Return-ext
+/*
+  These are hardwired PBX extensions of the format:
+  Called as getdtmf#NN#XX#YY#ZZZZ - NN = msgno, XX = timeout, YY = maxdigits, ZZZZ = Return-ext
+*/
 // Play message 70, timeout 10 secs, maxdigits 8, back to queue
-const INPUTDOB = 'getdtmf#70#10#8#227';
-const DOBERROR = 'getdtmf#71#1#1#227'
+const INPUTDOB = 'getdtmf#70#10#8#777';
+// Play message 71 (error message) timeout after 1 sec to queue
+const DOBERROR = 'getdtmf#71#1#1#777'
 var callState = {};
-var age;
+
+
 function cbCall(call) {
-  console.log(call);
-  if(!(state = callState[call.ID]))
-    callState[call.ID] = 'discovered';
+  if(!call)
+    call = this;
+
+  if (call != null && !call.id) {
+    console.log('err this seems to be a call but has a null ID', call);
+    return;
+  }
+
+  var age;
+
+  console.log('cbCall:', call.id, 'input state', callState[call.id], call.number);
 
 
-  switch (callState[call.ID]){
+  if (!(state = callState[call.id]))
+    callState[call.id] = 'discovered';
+
+
+
+  switch (callState[call.id]) {
+    // We just out put an error message and now we are back in the queue
+    case 'errorMessage':
+      if (!call.number.match('getdtmf'))
+        callState[call.id] = 'discovered';
+      else
+        break;
+      // Newly discovered call, send it to collect DOB DTMF
     case 'discovered':
-      console.log('got update in inputDOB');
-      call.sendto(INPUTDOB);
-      callState[call.ID] = 'inputDOB';
+      call.send(INPUTDOB, (e, txt) => console.log('send', e, txt));
+      callState[call.id] = 'inputDOB';
       break;
 
     case 'inputDOB':
-      console.log('got update in inputDOB');
+      if (call.number.match('getdtmf'))
+        callState[call.id] = 'gettingDOB';
+      break;
 
-      if (call.DTMF != ''){
-        if((age = processDOB(call.DTMF)) > 65){
-          callState[call.ID] = 'done';
-          call.sento('over65');
-          break;
-        }
-        else if (age > 18){
-          callState[call.ID] = 'done';
-          call.sento('over18');
-          break;
-        }
-        else if (age > 0){
-          callState[call.ID] = 'done';
-          call.sento('tooyoung');
-          break;
-        }
-        else {
-          callState[call.ID] = 'inputError';
-          call.sento('DOBERROR');
+    case 'gettingDOB':
+      // We were getting DOB and now we are back in the queue so that means
+      // we have all the dTmf we are going to get
+      if (!call.number.match('getdtmf')) {
+        if (call.dtmfString && call.dtmfString.length) {
+          console.log('dtmfString:', call.dtmfString);
+          if ((age = processDOB(call.dtmfString)) > 65) {
+            callState[call.id] = 'done';
+            call.send('over65', (e, txt) => console.log('send', e, txt));
+            break;
+          } else if (age > 18) {
+            callState[call.id] = 'done';
+            console.log('sending to over18');
+            call.send('over18', (e, txt) => console.log('send', e, txt));
+            break;
+          } else if (age > 0) {
+            callState[call.id] = 'done';
+            call.send('tooyoung', (e, txt) => console.log('send', e, txt));
+            break;
+          } else {
+            callState[call.id] = 'inputError';
+            call.send(DOBERROR, (e, txt) => console.log('send', e, txt));
+            break;
+          }
+        } else {
+          callState[call.id] = 'inputError';
+          call.send(DOBERROR, (e, txt) => console.log('send', e, txt));
           break;
         }
       }
       break;
 
-      case 'inputError':
-        console.log('got update in inputError');
-        break;
+    case 'inputError':
+      // We are outputting the error message
+      if (call.number.match('getdtmf'))
+        callState[call.id] = 'errorMessage';
+      break;
 
-      case 'done':
-        console.log('got update in inputError');
-      default:
-        console.log('fallthrough', callState[call.ID])
-        break;
-      }
 
-    }
+    case 'done':
+      // We have done everything we can with this call now
+    default:
+      break;
+  }
 
-    function processDOB(str){
-      return(-1);
-    }
+  console.log('cbCall:', call.id, 'exit state', callState[call.id]);
+
+}
+
+function processDOB(str) {
+  var dobar = str.match(/([0-9][0-9])([0-9][0-9])([0-9][0-9]*)/)
+  if(!dobar || dobar.length != 4)
+    return(-1)
+  var dob = new Date(dobar[3], dobar[2], dobar[1]);
+  var today = new Date();
+  var age = today.getFullYear() - dob.getFullYear();
+  if (today.getMonth() < dob.getMonth || (today.getMonth() == dob.getMonth() && today.getDate() < today.getDate()))
+    age--;
+  console.log('returning age:', age);
+  return ((age > 1) ? age : -1);
+}
