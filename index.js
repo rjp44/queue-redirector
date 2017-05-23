2e28cc820fa0c309c678f5be6f9fff8231db73c6
Rob pickering <rob@ipcortex.co.uk>
2017-05-23 11:26:13 +0100

    First pass at new structure

--- index.js
+++ index.js
@@ -20,17 +20,77 @@ IPCortex.PBX.Auth.login({
   console.log('Login failed');
 });
 
-function callbackCall(queue) {
+function cbQueue(queue) {
   console.log('got Queue', queue.name, queue.depth, queue.calls);
 
   for (var c in queue.calls) {
     call = queue.calls[c];
     console.log('got Call', call.id, call.state, call.name);
-    call.addListener
-    if (call.state === 'up' && !id)
-      call.xfer('201', e => {
-        console.log(e)
-      });
+    call.addListener('update', cbCall)
+
   }
 
 }
+
+// Called as getdtmf#NN#XX#YY#ZZZZ - NN = msgno, XX = timeout, YY = maxdigits, ZZZZ = Return-ext
+// Play message 70, timeout 10 secs, maxdigits 8, back to queue
+const INPUTDOB = 'getdtmf#70#10#8#227';
+const DOBERROR = 'getdtmf#71#1#1#227'
+var callState = {};
+var age;
+function cbCall(call) {
+  console.log(call);
+  if(!(state = callState[call.ID]))
+    callState[call.ID] = 'discovered';
+
+
+  switch (callState[call.ID]){
+    case 'discovered':
+      console.log('got update in inputDOB');
+      call.sendto(INPUTDOB);
+      callState[call.ID] = 'inputDOB';
+      break;
+
+    case 'inputDOB':
+      console.log('got update in inputDOB');
+
+      if (call.DTMF != ''){
+        if((age = processDOB(call.DTMF)) > 65){
+          callState[call.ID] = 'done';
+          call.sento('over65');
+          break;
+        }
+        else if (age > 18){
+          callState[call.ID] = 'done';
+          call.sento('over18');
+          break;
+        }
+        else if (age > 0){
+          callState[call.ID] = 'done';
+          call.sento('tooyoung');
+          break;
+        }
+        else {
+          callState[call.ID] = 'inputError';
+          call.sento('DOBERROR');
+          break;
+        }
+      }
+      break;
+
+      case 'inputError':
+        console.log('got update in inputError');
+        break;
+
+      case 'done':
+        console.log('got update in inputError');
+      default:
+        console.log('fallthrough', callState[call.ID])
+        break;
+      }
+
+    }
+
+    function processDOB(str){
+      return(-1);
+    }
